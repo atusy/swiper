27b99d6cfa07f388de21baaeb09bd6bfbe1c6a3a
atusy <30277794+atusy@users.noreply.github.com>
2021-06-26 03:17:47 +0900

    nice start

---
+++ R/swiper_presentation.R
@@ -0,0 +1,33 @@
+#' @export
+swiper_presentation <- function(
+  keep_md = FALSE,
+  self_contained = FALSE,
+  ...
+) {
+  rmarkdown::output_format(
+    knitr = NULL,
+    pandoc = rmarkdown::pandoc_options(
+      to = "html5",
+      args = c(
+        "--include-in-header", swiper_file("header.html"),
+        "--include-before-body", swiper_file("before-body.html"),
+        "--include-after-body", swiper_file("after-body.html"),
+        "--lua-filter", swiper_file("swiper.lua")
+      )
+    ),
+    keep_md = keep_md,
+    clean_supporting = !self_contained,
+    base_format = rmarkdown::html_vignette(
+      template = NULL,
+      mathjax = NULL
+    )
+  )
+}
+
+swiper_file <- function(...) {
+  system.file(
+    package = "swiper",
+    "rmarkdown/templates/swiper_presentation",
+    ...
+  )
+}
